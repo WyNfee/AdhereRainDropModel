d2f39d502b241fc2c2a78009432e10b729b66925
WyNfee <wangyifei52199@sina.com>
2018-10-22 17:42:20 +0800

    remove the project

--- Common/runshader.bat
+++
@@ -1,8 +0,0 @@
-REM %1% -- Command line
-REM %2% -- OUTPUT folder
-REM %3% -- Shader model
-REM %4% -- Entry point
-REM %5% -- Shader file name (including extension)
-REM %6% -- HLSL source
-
-%~1 /T%3 /E%4 /Qstrip_debug /Fc"%~2Media\Shaders\%5.lst" /Fo"%~2Media\Shaders\%5" /Fd"%~2Media\Shaders\%5.pdb" %6
