cb0df59339ce8b9cae045424e4377356dea51110
WyNfee <wangyifei52199@sina.com>
2018-10-23 20:22:44 +0800

    new method to generate cubic spine line curve

--- met_file_reader.py
+++ met_file_reader.py
@@ -7,8 +7,12 @@ Description: This script will extract the data in met file for adhere rain drop 
 import cv2
 import numpy as np
 from collections import namedtuple
+import bezier_curve
+import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
 
 Point = namedtuple("Point", ["X", "Y", "Color"])
+ControlPoints = namedtuple("ControlPoints", ["HeightCurve", "HeightPeak","ShapeCurveA", "ShapeCurveB"])
 
 COLOR_TYPE_DICT = {
     "RED": 1,
@@ -103,10 +107,115 @@ def _plot_data_point(data_points, height, width):
     cv2.waitKey(-1)
 
 
-met_data = read_met_file(r"C:\Users\wangy\Documents\GitHub\AdhereRainDropModel\rain_drop_data.bmp")
+def _sort_points_by_x_axis(e):
+    return e.X
+
+
+def _generate_curve_common_control_points(colored_points):
+    points_list = colored_points
+
+    points_list.sort(key=_sort_points_by_x_axis)
+
+    x_list = [p.X for p in points_list]
+    y_list = [p.Y for p in points_list]
+
+    max_x = max(x_list)
+    min_x = min(x_list)
+    width = max_x - min_x
+
+    max_y = max(y_list)
+    min_y = min(y_list)
+    height = max_y - min_y
+
+    x_list = [(x - min_x) / width for x in x_list]
+    y_list = [(max_y - y) / height for y in y_list]
+
+    control_points = list()
+
+    for idx in range(len(x_list)):
+        control_points.append((x_list[idx], y_list[idx]))
+
+    return control_points
+
+
+def _plot_common_cubic_curve(control_points):
+    x_coord = [p[0] for p in control_points]
+    y_coord = [p[1] for p in control_points]
+    cs = CubicSpline(x_coord, y_coord)
+    xs = np.arange(0, 1, 0.001)
+    plt.plot(x_coord, y_coord, 'o', label='data')
+    plt.plot(xs, cs(xs), label="S")
+    plt.show()
+
+
+def _generate_curve_shape_control_points(colored_points):
+    points_list = colored_points
+    points_list.sort(key=_sort_points_by_x_axis)
+
+    x_list = [p.X for p in points_list]
+    y_list = [p.Y for p in points_list]
+
+    max_x = max(x_list)
+    min_x = min(x_list)
+    width = max_x - min_x
+
+    max_y = max(y_list)
+    min_y = min(y_list)
+    height = max_y - min_y
+
+    x_list = [((x - min_x)/width - 0.5) * 2 for x in x_list]  # convert x_list to [-1, 1]
+    y_list = [((y - min_y)/height - 0.5) * 2 for y in y_list] # convert y to [-1,1]
+
+    access_list = [0, 1, 3, 5, 7, 9, 8, 6, 4, 2, 0]
+
+    check_point_list = list()
+
+    for acc_idx in access_list:
+        check_point_list.append((x_list[acc_idx], y_list[acc_idx]))
+
+    return check_point_list
+
+
+def _plot_shape_cubic_curve(shape_curve):
+    theta = 2 * np.pi * np.linspace(0, 1, 11)
+    cs = CubicSpline(theta, shape_curve, bc_type='periodic')
+    xs = 2 * np.pi * np.linspace(0, 1, 200)
+    plt.plot(cs(xs)[:, 0], cs(xs)[:, 1], label='spline')
+    plt.show()
+
+
+def convert_data_points_to_control_points(data_points):
+    green_points = list()
+    black_points = list()
+    red_points = list()
+    blue_points = list()
+
+    for point in data_points:
+        pc = point.Color
+        if pc == 1:
+            red_points.append(point)
+        elif pc == 2:
+            blue_points.append(point)
+        elif pc == 3:
+            black_points.append(point)
+        elif pc == 5:
+            green_points.append(point)
+        else:
+            raise ValueError("Not support color exists in color points")
+
+    height_curve = _generate_curve_common_control_points(red_points)
+    height_peak = _generate_curve_common_control_points(blue_points)
+    shape_curve = _generate_curve_shape_control_points(black_points)
+
+    _plot_common_cubic_curve(height_curve)
+    _plot_common_cubic_curve(height_peak)
+    _plot_shape_cubic_curve(shape_curve)
+
+
+met_data = read_met_file(r"C:\Users\wangy\Documents\GitHub\AdhereRainDropModel\test.bmp")
 valid_data_point = extract_data(met_data)
 _plot_data_point(valid_data_point, met_data.shape[0], met_data.shape[1])
-
+convert_data_points_to_control_points(valid_data_point)
 
 cv2.imshow("data", met_data)
 cv2.waitKey(-1)
