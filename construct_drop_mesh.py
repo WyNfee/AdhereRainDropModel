3a6a120e37f5d51be82b47d8cd792b2ad1e5aada
WyNfee <wangyifei52199@sina.com>
2018-10-31 15:34:02 +0800

    able to generate normal based on mesh

--- construct_drop_mesh.py
+++ construct_drop_mesh.py
@@ -99,6 +99,95 @@ def _abstract_sample_data(x_data, y_data, p_height_data, p_shape_data,
 
         plt.show()
 
+    return abstracted_data
+
+
+def _normalize_vector(input_vector):
+    """
+    normalize an input vector to a module of 1
+    :param input_vector: the vector
+    :return: the normalized vector
+    """
+    norm = np.linalg.norm(input_vector)
+    norm_vec = input_vector / (norm + 1e-6)
+    return norm_vec
+
+
+def _compute_normal(mesh_data, current_h_idx, current_w_idx):
+    """
+    compute the normal of a mesh point, by sample its adjacent other points
+    :param mesh_data: mesh data to work on
+    :param current_h_idx: current mesh index on y axis
+    :param current_w_idx: current mesh index on x axix (assuming z is for height)
+    :return: the normal compute out
+    """
+    current_pt = mesh_data[current_h_idx][current_w_idx]
+    left_pt = mesh_data[current_h_idx][current_w_idx - 1]
+    right_pt = mesh_data[current_h_idx][current_w_idx + 1]
+    bottom_pt = mesh_data[current_h_idx + 1][current_w_idx]
+    up_pt = mesh_data[current_h_idx - 1][current_w_idx]
+
+    left_vector = current_pt - left_pt
+    right_vector = right_pt - current_pt
+    up_vector = current_pt - up_pt
+    bottom_vector = bottom_pt - current_pt
+
+    left_up_normal = np.cross(left_vector, up_vector)
+    left_bottom_normal = np.cross(left_vector, bottom_vector)
+    right_up_normal = np.cross(right_vector, up_vector)
+    right_bottom_normal = np.cross(right_vector, bottom_vector)
+
+    left_up_normal = _normalize_vector(left_up_normal)
+    left_bottom_normal = _normalize_vector(left_bottom_normal)
+    right_up_normal = _normalize_vector(right_up_normal)
+    right_bottom_normal = _normalize_vector(right_bottom_normal)
+
+    final_normal = (left_up_normal + left_bottom_normal + right_up_normal + right_bottom_normal) / 4
+
+    final_normal = _normalize_vector(final_normal)
+
+    return final_normal
+
+
+def compute_normal_based_on_mesh(mesh, enable_debugging=False):
+    """
+    compute the normal based on the mesh created
+    :param mesh: the mesh to work on
+    :param enable_debugging: whether enable debugging plot
+    :return:
+    """
+    height = mesh.shape[0]
+    width = mesh.shape[1]
+
+    normal_data = np.zeros_like(mesh)
+
+    for h_idx in range(height):
+        for w_idx in range(width):
+            if 0 < h_idx < height-1 and 0 < w_idx < width-1:
+                norm = _compute_normal(mesh, h_idx, w_idx)
+                normal_data[h_idx][w_idx] = norm
+
+    if enable_debugging is True:
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.set_title("rain drop Mesh - 3D")
+        ax.set_xlabel('X axis')
+        ax.set_ylabel('Y axis')
+        ax.set_zlabel('Z axis')
+        ax.set_xlim(0, 100)
+        ax.set_ylim(-50, 50)
+        ax.set_zlim(0, 100)
+        ax.plot_wireframe(mesh[:, :, 0], mesh[:, :, 1], mesh[:, :, 2], color='b')
+        for m_h_idx in range(height):
+            for m_w_idx in range(width):
+                if m_h_idx % 100 == 0 and m_w_idx % 100 == 0:
+                    normal = normal_data[m_h_idx][m_w_idx] * 5
+                    point = mesh[m_h_idx, m_w_idx]
+                    ax.quiver(point[0], point[1], point[2], normal[0], normal[1], normal[2], color='r')
+        plt.show()
+
+    return normal_data
+
 
 def load_sample_data(shape_x, shape_y, peak_height, peak_shape):
     """
@@ -119,7 +208,12 @@ def load_sample_data(shape_x, shape_y, peak_height, peak_shape):
 
 data_x, data_y, data_ph, data_ps = load_sample_data(FILE_OUT_SHAPE_X, FILE_OUT_SHAPE_Y, FILE_PEAK_HEIGHT, FILE_PEAK_SHAPE)
 selected_index = _random_generate_pick_index(data_x)
-_abstract_sample_data(data_x[selected_index], data_y[selected_index], data_ph[selected_index], data_ps[selected_index],
-                      100, 80, 12, 0.1,
-                      enable_debugging=True)
+model_data = _abstract_sample_data(data_x[selected_index], data_y[selected_index], data_ph[selected_index], data_ps[selected_index],
+                                   40, 80, 12, 0.1,
+                                   enable_debugging=False)
+# this operation is really time consuming!!!
+normal_data = compute_normal_based_on_mesh(model_data, enable_debugging=True)
+
+
+
 print("complete!")
