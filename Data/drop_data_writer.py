7aebf8c3fa9bee2420b5e8f5ae851a5b7673fd4b
WyNfee <wangyifei52199@sina.com>
2018-11-05 14:15:00 +0800

    repalce numpy data mask data by jpg file, reduce the file size (8M -> < 200K)

--- Data/drop_data_writer.py
+++ Data/drop_data_writer.py
@@ -11,10 +11,13 @@ import tensorflow as tf
 
 
 # the directory of drop data to read
-DROP_DATA_DIR = r"C:\Users\wangy\Documents\GitHub\AdhereRainDropModel\_Raw\drop"
+DROP_DATA_DIR = r"D:\Data\TFTrain\AdhereRainDrop\Drop"
 
 # the output file path
-OUTPUT_TFRECORD = r"C:\Users\wangy\Documents\GitHub\AdhereRainDropModel\_Train\record\drop.tfrecords"
+OUTPUT_TFRECORD = r"D:\Data\TFTrain\AdhereRainDrop\Record\drop.tfrecords"
+
+# whether enable debugging
+ENABLE_DEBUGGING = False
 
 
 def _bytes_feature(value):
@@ -26,29 +29,29 @@ def read_the_drop_data(drop_data_directory):
     drop_data_list = list()
 
     for file in file_list:
-        if file.endswith(".npy") is True and os.path.exists(os.path.join(drop_data_directory, file.split(".")[0] + ".jpg")) is True:
-            file_path_to_store = os.path.join(drop_data_directory, file.split(".")[0])
+        is_mask_data = file.find("_mask") != 0
+        has_base_data = os.path.exists(os.path.join(drop_data_directory, file.split(".")[0].split("_")[0] + ".jpg"))
+        if is_mask_data is True and has_base_data is True:
+            file_path_to_store = os.path.join(drop_data_directory, file.split(".")[0].split("_")[0])
             drop_data_list.append(file_path_to_store)
 
     return drop_data_list
 
 
-def preprocess_drop_data(drop_data, temp_dir):
+def preprocess_drop_data(drop_data):
     image_path = drop_data + '.jpg'
-    mask_path = drop_data + '.npy'
+    mask_path = drop_data + '_mask.jpg'
 
     image_data = cv2.imread(image_path)
-    mask_data = np.load(mask_path)
-
-    mask_data = mask_data.astype(dtype=np.uint8)
-
-    image_data = image_data[80:, :]
-    mask_data = mask_data[80:, :]
-
-    mask_data = np.stack([mask_data, mask_data, mask_data], axis=-1)
+    mask_data = cv2.imread(mask_path)
 
     input_image_data = cv2.resize(image_data, (320, 160))
     input_mask_data = cv2.resize(mask_data, (40, 20))
+
+    if ENABLE_DEBUGGING:
+        cv2.imshow("input_image", input_image_data)
+        cv2.imshow("input_mask", input_mask_data)
+        cv2.waitKey(-1)
 
     return input_image_data, input_mask_data
 
@@ -60,7 +63,7 @@ def generate_tfrecords(drop_data_dir, output_file):
     example_amount = len(data_path_list)
 
     for idx, data_path in enumerate(data_path_list):
-        image_data, mask_data = preprocess_drop_data(data_path, drop_data_dir)
+        image_data, mask_data = preprocess_drop_data(data_path)
         example = tf.train.Example(features=tf.train.Features(
             feature={
                 'image': _bytes_feature(image_data.tostring()),
