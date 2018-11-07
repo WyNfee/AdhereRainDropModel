ac743809cec02b958a8ace56848f20a9bf9502f9
WyNfee <wangyifei52199@sina.com>
2018-11-07 10:54:27 +0800

    update water drop generation methods

--- Drop/draw_drop_on_image.py
+++ Drop/draw_drop_on_image.py
@@ -10,23 +10,25 @@ import numpy as np
 from Drop import construct_drop_mesh as mesh_builder
 import copy
 
-BACKGROUND_BLUR_KERNEL_RANGE = (2, 7)
+BACKGROUND_BLUR_KERNEL_RANGE = (5, 15)
 
-BOUNDARY_BLUR_KERNEL_RANGE = (8, 15)
+BOUNDARY_BLUR_KERNEL_RANGE = (18, 50)
 
-BLEND_RANGE = (0.2, 0.8)
+BLEND_RANGE = (0.75, 0.95)
 
-WATER_DROP_AMOUNT_RANGE = (1, 4)
+WATER_DROP_AMOUNT_RANGE = (2, 5)
 
-WATER_DROP_SIZE_RANGE = (100, 250)
+WATER_DROP_SIZE_RANGE = (60, 100)
 
-WATER_DROP_SHAPE_OFFSET_RANGE = (0.1, 0.4)
+WATER_DROP_SHAPE_OFFSET_RANGE = (0.4, 0.8)
 
-WATER_DROP_HEIGHT_RANGE = (60, 100)
+WATER_DROP_HEIGHT_RANGE = (60, 200)
 
 WATER_DROP_LOCATION_X = (0, 1000)
 
 WATER_DROP_LOCATION_Y = (0, 550)
+
+LIGHT_INTENSITY_RANGE = (1.1, 1.5)
 
 DROP_MAX_TRIAL = 200
 
@@ -48,7 +50,11 @@ def read_processing_image(image_file_path):
     # print("reference kernel size %d" % gbk)
     reference_image = cv2.GaussianBlur(reference_image, (gbk, gbk), 0)
     reference_image = np.flipud(reference_image)
-    # reference_image = (reference_image * 0.8).astype(np.uint8)
+    # the reason of multiple light intensity (should larger than 1)
+    # that is the lux in world is much larger than those in photo graphs
+    light_intensity = int(random.uniform(LIGHT_INTENSITY_RANGE[0], LIGHT_INTENSITY_RANGE[1]))
+    reference_image = reference_image * light_intensity
+    reference_image = np.clip(reference_image, 0, 255).astype(np.uint8)
 
     return process_image, reference_image
 
@@ -182,7 +188,8 @@ def create_rain_drop_on_image(output_image, drop_mask, input_image, reference_im
                 # if z value is small, mean x or y is strong, then use reference value more
                 # other wise, use current value more
                 # translate_factor = normal_z  # * blend_factor
-                drop_pixel = reference_value * normal_z + current_value * (1 - normal_z) * blend_factor
+                drop_pixel = reference_value * (1 - normal_z) * blend_factor + current_value * normal_z
+                drop_pixel = np.clip(drop_pixel, 0, 255)
 
                 # apply the drop pixel and record the mask of the image
                 water_drop_paste[pixel_loc_y][pixel_loc_x] = drop_pixel
@@ -203,11 +210,11 @@ def create_rain_drop_on_image(output_image, drop_mask, input_image, reference_im
     fill_area_height = max_pixel_loc_y - min_pixel_loc_y
     fill_area_width = max_pixel_loc_x - min_pixel_loc_x
 
-    min_pixel_loc_y = min_pixel_loc_y - int(fill_area_height / 5)
-    min_pixel_loc_x = min_pixel_loc_x - int(fill_area_width / 5)
+    min_pixel_loc_y = min_pixel_loc_y - int(fill_area_height / 2)
+    min_pixel_loc_x = min_pixel_loc_x - int(fill_area_width / 2)
 
-    max_pixel_loc_y = max_pixel_loc_y + int(fill_area_height / 5)
-    max_pixel_loc_x = max_pixel_loc_x + int(fill_area_width / 5)
+    max_pixel_loc_y = max_pixel_loc_y + int(fill_area_height / 2)
+    max_pixel_loc_x = max_pixel_loc_x + int(fill_area_width / 2)
 
     fill_area_height = max_pixel_loc_y - min_pixel_loc_y
     fill_area_width = max_pixel_loc_x - min_pixel_loc_x
@@ -252,75 +259,127 @@ def create_rain_drop_on_image(output_image, drop_mask, input_image, reference_im
     return output_image, drop_mask
 
 
+def rotate_bound(image, angle):
+    # grab the dimensions of the image and then determine the
+    # center
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+
+    # adjust the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+
+    # perform the actual rotation and return the image
+    return cv2.warpAffine(image, M, (nW, nH))
+
+
 def create_water_drops_on_image(output_image, drop_mask, image_data, reference_image, data_x, data_y, data_ph, data_ps):
     water_drop_amount = int(random.uniform(WATER_DROP_AMOUNT_RANGE[0], WATER_DROP_AMOUNT_RANGE[1]))
     # print("Generating %d drops on image" % water_drop_amount)
     water_drop_idx = 0
 
     water_drop_location_list = list()
+    water_drop_parameter_list = list()
 
     print("About to generate %d drops on image" % water_drop_amount)
-    while water_drop_idx < water_drop_amount:
-        is_valid_location = False
-        trial_counter = 0
-        while is_valid_location is False and trial_counter < DROP_MAX_TRIAL:
-            water_drop_size_x = int(random.uniform(WATER_DROP_SIZE_RANGE[0], WATER_DROP_SIZE_RANGE[1]))
-            water_drop_size_y = int(random.uniform(WATER_DROP_SIZE_RANGE[0], WATER_DROP_SIZE_RANGE[1]))
+    trial_counter = 0
+    while water_drop_idx < water_drop_amount and trial_counter < DROP_MAX_TRIAL:
+        water_drop_size_x = int(random.uniform(WATER_DROP_SIZE_RANGE[0], WATER_DROP_SIZE_RANGE[1]))
+        water_drop_size_y = int(random.uniform(WATER_DROP_SIZE_RANGE[0], WATER_DROP_SIZE_RANGE[1]))
 
-            water_drop_height = int(random.uniform(WATER_DROP_HEIGHT_RANGE[0], WATER_DROP_HEIGHT_RANGE[1]))
+        water_drop_height = int(random.uniform(WATER_DROP_HEIGHT_RANGE[0], WATER_DROP_HEIGHT_RANGE[1]))
 
-            water_drop_shape = random.uniform(WATER_DROP_SHAPE_OFFSET_RANGE[0], WATER_DROP_SHAPE_OFFSET_RANGE[1])
-            water_drop_loc_x = int(random.uniform(WATER_DROP_LOCATION_X[0], WATER_DROP_LOCATION_X[1]))
-            water_drop_loc_y = int(random.uniform(WATER_DROP_LOCATION_Y[0], WATER_DROP_LOCATION_Y[1]))
+        water_drop_shape = random.uniform(WATER_DROP_SHAPE_OFFSET_RANGE[0], WATER_DROP_SHAPE_OFFSET_RANGE[1])
+        water_drop_loc_x = int(random.uniform(WATER_DROP_LOCATION_X[0], WATER_DROP_LOCATION_X[1]))
+        water_drop_loc_y = int(random.uniform(WATER_DROP_LOCATION_Y[0], WATER_DROP_LOCATION_Y[1]))
 
-            # roughly estimate whether this place is occupied or not
-            drop_xmin = water_drop_loc_x
-            drop_ymin = water_drop_loc_y
-            drop_xmax = water_drop_loc_x + water_drop_size_x
-            drop_ymax = water_drop_loc_y + water_drop_size_y
+        # roughly estimate whether this place is occupied or not
+        # since we are about to rotate the drop at the place, reserve the largest space for it...
+        # not ideal...
+        drop_xmin = water_drop_loc_x
+        drop_ymin = water_drop_loc_y
+        drop_xmax = water_drop_loc_x + water_drop_size_x
+        drop_ymax = water_drop_loc_y + water_drop_size_y
 
-            if len(water_drop_location_list) == 0:
-                is_valid_location = True
-                water_drop_location_list.append((drop_xmin, drop_ymin, drop_xmax, drop_ymax))
-                break
-            else:
-                for loc in water_drop_location_list:
-                    loc_xmin = loc[0]
-                    loc_ymin = loc[1]
-                    loc_xmax = loc[2]
-                    loc_ymax = loc[3]
+        drop_x_center = (drop_xmax + drop_xmin) / 2
+        drop_y_center = (drop_ymax + drop_ymin) / 2
+        drop_side = max([water_drop_size_y, water_drop_size_x])
+        drop_side = drop_side * 3
 
-                    xmin = max([drop_xmin, loc_xmin])
-                    xmax = min([drop_xmax, loc_xmax])
-                    ymin = max([drop_ymin, loc_ymin])
-                    ymax = min([drop_ymax, loc_ymax])
+        drop_test_xmin = drop_x_center - drop_side / 2
+        drop_test_xmax = drop_x_center + drop_side / 2
+        drop_test_ymin = drop_y_center - drop_side / 2
+        drop_test_ymax = drop_y_center + drop_side / 2
 
-                    if xmax <= xmin or ymax <= ymin:
-                        is_valid_location = True
-                        water_drop_location_list.append((drop_xmin, drop_ymin, drop_xmax, drop_ymax))
-                        break
+        if len(water_drop_location_list) == 0:
+            water_drop_location_list.append((drop_xmin, drop_ymin, drop_xmax, drop_ymax))
+            print("%.4f, %.4f, %.4f, %.4f" % (drop_xmin, drop_ymin, drop_xmax, drop_ymax))
+            water_drop_parameter_list.append((water_drop_size_x, water_drop_size_y, water_drop_height, water_drop_shape, water_drop_loc_x, water_drop_loc_y))
+            water_drop_idx += 1
+            trial_counter += 1
+            continue
+        else:
+            for loc in water_drop_location_list:
+                loc_xmin = loc[0]
+                loc_ymin = loc[1]
+                loc_xmax = loc[2]
+                loc_ymax = loc[3]
+
+                loc_x_center = (loc_xmin + loc_xmax) / 2
+                loc_y_center = (loc_ymin + loc_ymax) / 2
+                loc_side = max([loc_ymax - loc_ymin, loc_xmax - loc_xmin])
+                loc_side = loc_side * 3
+
+                loc_test_xmin = loc_x_center - loc_side / 2
+                loc_test_xmax = loc_x_center + loc_side / 2
+                loc_test_ymin = loc_y_center - loc_side / 2
+                loc_test_ymax = loc_y_center + loc_side / 2
+
+                xmin = max([drop_test_xmin, loc_test_xmin])
+                xmax = min([drop_test_xmax, loc_test_xmax])
+                ymin = max([drop_test_ymin, loc_test_ymin])
+                ymax = min([drop_test_ymax, loc_test_ymax])
+
+                if xmax <= xmin and ymax <= ymin:
+                    water_drop_location_list.append((drop_xmin, drop_ymin, drop_xmax, drop_ymax))
+                    print("%.4f, %.4f, %.4f, %.4f" % (drop_xmin, drop_ymin, drop_xmax, drop_ymax))
+                    water_drop_parameter_list.append((water_drop_size_x, water_drop_size_y, water_drop_height, water_drop_shape, water_drop_loc_x, water_drop_loc_y))
+                    water_drop_idx += 1
+                    trial_counter += 1
+                    continue
 
             trial_counter += 1
 
-        if is_valid_location is True:
-            blend_range = BLEND_RANGE
+    print("Total will generate %d" % len(water_drop_parameter_list))
+    for water_drop_idx, parameter in enumerate(water_drop_parameter_list):
+        blend_range = BLEND_RANGE
 
-            selected_index = mesh_builder._random_generate_pick_index(data_x)
-            model_data = mesh_builder._abstract_sample_data(data_x[selected_index], data_y[selected_index], data_ph[selected_index], data_ps[selected_index],
-                                                            water_drop_size_y, water_drop_size_x, water_drop_height, water_drop_shape,
-                                                            enable_debugging=False)
+        selected_index = mesh_builder._random_generate_pick_index(data_x)
+        model_data = mesh_builder._abstract_sample_data(data_x[selected_index], data_y[selected_index], data_ph[selected_index], data_ps[selected_index],
+                                                        parameter[1], parameter[0], parameter[2], parameter[3],
+                                                        enable_debugging=False)
 
-            project_mesh = project_drop_model_to_image_space(model_data)
-            project_normal = mesh_builder.compute_normal_based_on_mesh(project_mesh, enable_debugging=False)
-            output_image, drop_mask = create_rain_drop_on_image(output_image, drop_mask,
-                                                                image_data, reference_image, project_normal,
-                                                                (water_drop_loc_y, water_drop_loc_x), blend_range)
+        project_mesh = project_drop_model_to_image_space(model_data)
+        project_normal = mesh_builder.compute_normal_based_on_mesh(project_mesh, enable_debugging=False)
+        # random rotation
+        rotate_degree = int(random.uniform(0, 360))
+        rotated_normal = rotate_bound(project_normal, rotate_degree)
+        output_image, drop_mask = create_rain_drop_on_image(output_image, drop_mask,
+                                                            image_data, reference_image, rotated_normal,
+                                                            (parameter[5], parameter[4]), blend_range)
 
-            water_drop_idx += 1
-            print("Drop %d has been generated" % water_drop_idx)
-
-        """while is_valid_location is False and trial_counter < DROP_MAX_TRIAL"""
-    """while water_drop_idx < water_drop_amount:"""
+        print("Drop %d has been generated" % water_drop_idx)
 
     return output_image, drop_mask
 
